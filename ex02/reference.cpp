09cb696e9e4690fbd6166e70065cc72d4df8dde3
Oscar Moreno Sancho <oscar.moreno.sancho@gmail.com>
2023-10-18 21:16:16 +0200

    ex02 reference sorts right

--- ex02/reference.cpp
+++ ex02/reference.cpp
@@ -6,12 +6,13 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:55:01 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/10/17 00:51:15 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/10/18 20:51:33 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <iostream>
 
@@ -42,6 +43,8 @@ Container& copyOfRange(Iterator begin, Iterator end)
 template <class Iterator, class Container>
 void insertionSort(Container& A, int p, int q)
 {
+	// std::cout << "\t\tA before insertion sort: ";
+	// print_vector(A.begin(), A.end());
 	for (int i = p; i < q; i++)
 	{
 		int tempVal = A[i + 1];
@@ -52,10 +55,13 @@ void insertionSort(Container& A, int p, int q)
 		}
 		A[j] = tempVal;
 	}
-	Container& temp = copyOfRange<Iterator, Container>
-						((A.begin() + p), (A.begin() + q + 1));
-	print_vector(temp.begin(), temp.end());
-	delete &temp;
+	// std::cout << "\t\tA after insertion sort: ";
+	// print_vector(A.begin(), A.end());
+	// Container& temp = copyOfRange<Iterator, Container>
+	// 					((A.begin() + p), (A.begin() + q + 1));
+	// std::cout << "\t\ttemp after insertion sort: ";
+	// print_vector(temp.begin(), temp.end());
+	// delete &temp;
 }
 
 template <class Iterator, class Container>
@@ -67,36 +73,63 @@ void merge(Container& A, int p, int q, int r)
 								((A.begin() + p), (A.begin() + q + 1));
 	Container& RA = copyOfRange<Iterator, Container>
 								((A.begin() + q + 1), (A.begin() + r + 1));
+	// std::cout << "\tmerging\n";
+	// std::cout << "\tLA: ";
+	// print_vector(LA.begin(), LA.end());
+	// std::cout << "\tRA: ";
+	// print_vector(RA.begin(), RA.end());
 	int RIDX = 0;
 	int LIDX = 0;
-	for (int i = p; i < r - p + 1; i++) {
-		if (RIDX == n2) {
+	// std::cout << "\tfrom p: " << p << " to r: " << r << "\n";
+	for (int i = p; i < r + 1; i++) {
+		if (RIDX == n2)
+		{
+			// std::cout << "i: " << i << "/ R reach end: " << n2 << ", copy LA["<< LIDX << "] with " << LA[LIDX] << " to A[" << i << "]\n";
 			A[i] = LA[LIDX];
 			LIDX++;
-		} else if (LIDX == n1) {
+		}
+		else if (LIDX == n1)
+		{
+			// std::cout << "i: " << i << "/ L reach end: " << n1 << ", copy RA["<< RIDX << "] with " << RA[RIDX] << " to A[" << i << "]\n";
 			A[i] = RA[RIDX];
 			RIDX++;
-		} else if (RA[RIDX] > LA[LIDX]) {
+		}
+		else if (RA[RIDX] > LA[LIDX])
+		{
+			// std::cout << "i: " << i << "/ RA["<< RIDX << "] with " << RA[RIDX] << " > LA[" << LIDX << "] with " << LA[LIDX] << " copy LA[inx] to A[" << i << "] \n";
 			A[i] = LA[LIDX];
 			LIDX++;
-		} else {
+		}
+		else
+		{
+			// std::cout << "i: " << i << "/ RA["<< RIDX << "] with " << RA[RIDX] << " < LA[" << LIDX << "] with " << LA[LIDX] << " copy RA[inx] to A[" << i << "] \n";
 			A[i] = RA[RIDX];
 			RIDX++;
 		}
 	}
 	delete &LA;
 	delete &RA;
+	// std::cout << "\tA after merge : ";
+	// print_vector(A.begin() + p, A.begin() + r + 1);
 }
 
 template <class Iterator, class Container>
 void sort(Container& A, int p, int r) {
 	if (r - p > K) {
+		// std::cout << "A before sort-merge: ";
+		// print_vector(A.begin() + p, A.begin() + r + 1);
 		int q = (p + r) / 2;
 		sort<Iterator>(A, p, q);
 		sort<Iterator>(A, q + 1, r);
 		merge<Iterator>(A, p, q, r);
+		// std::cout << "A after: sort-merge ";
+		// print_vector(A.begin() + p, A.begin() + r + 1);
 	} else {
+		// std::cout << "A before insert sort: ";
+		// print_vector(A.begin() + p, A.begin() + r + 1);
 		insertionSort<Iterator>(A, p, r);
+		// std::cout << "A after insert sort: ";
+		// print_vector(A.begin() + p, A.begin() + r + 1);
 	}
 }
 
@@ -104,11 +137,17 @@ int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	int arrInt[] = { 2, 5, 1, 6, 7, 3, 8, 4, 9 };
+	// int arrInt[] = { 10, 2, 5, 1, 6, 7, 3, 8, 4, 9, 12, 15, 11, 16, 17, 13, 18, 14, 19};
+	// std::vector<int> A;
+	// unsigned int len = sizeof(arrInt)/sizeof(int);
+	// for (unsigned int i = 0; i < len; i++)
+	//  	A.push_back(arrInt[i]);
 	std::vector<int> A;
-	unsigned int len = sizeof(arrInt)/sizeof(int);
-	for (unsigned int i = 0; i < len; i++)
-	 	A.push_back(arrInt[i]);
+	for (int i = 1; i < argc; i++)
+	{
+		int temp = std::atoi(argv[i]);
+		A.push_back(temp);
+	}
 	std::cout << "at the begining: ";
 	print_vector(A.begin(), A.end());
 	sort<std::vector<int>::iterator>(A, 0, A.size() - 1);
