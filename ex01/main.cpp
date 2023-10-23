0996aee30ac86dded63f171f97e725ec0a6a808e
Oscar Moreno Sancho <omoreno-@car8s6.42barcelona.com>
2023-10-23 15:56:23 +0200

    ex01 colors in external file

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,27 +6,19 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 15:30:44 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/10/20 19:09:14 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/10/23 15:54:36 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <RPN.hpp>
 #include <iostream>
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
+#include <Colors.hpp>
 
 int main(int argc, char const *argv[])
 {
 	if (argc != 2)
 	{
-		std::cerr << "Error: Usage: ";
+		std::cerr << RED"Error: Usage: ";
 		std::cerr << argv[0] << " \"list_of_operands_or_operators\"";
 		std::cerr << std::endl;
 		return (1);
